0b3e8bbbaa0ddab0b6b725fed0c3f1d6e878b8fb
dragon-fire-fly <ek93@hotmail.co.uk>
2023-01-16 19:17:42 +0100

    Link CSS file to base template & update settings

--- developer_matcher/settings.py
+++ developer_matcher/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-fw0_xy6x64^uq+a6=49m#oach^ij(6*5pgz@g00ek+*_+2s0o2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
