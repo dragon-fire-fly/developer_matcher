da64956a54626b47259769e7f7dd486e22f8a2df
dragon-fire-fly <ek93@hotmail.co.uk>
2023-01-22 12:36:08 +0100

    Register additional user model fields in admin.py

--- app_home/admin.py
+++ app_home/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+# from django.contrib.auth.admin import UserAdmin
+from .models import User, UserProfilePicture
+
+
+class UserProfilePictureInLine(admin.TabularInline):
+    model = UserProfilePicture
+    extra = 0
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "location",
+        "github_user",
+        "github_url",
+        "linked_in",
+        "portfolio",
+        "date_joined",
+        "last_login",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+    ]
+    inlines = [UserProfilePictureInLine]
+
 
 admin.site.register(User, UserAdmin)
+
