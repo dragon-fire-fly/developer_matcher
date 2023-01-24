99fbac589ea278f2ed5599b07f5d946661e8d7e7
dragon-fire-fly <ek93@hotmail.co.uk>
2023-01-24 20:50:14 +0100

    Move content of admin.py from app_home to app_user

--- app_user/admin.py
+++ app_user/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+from .models import User, UserProfilePicture, Project, ProgrammingLanguage
 
-# Register your models here.
+
+class UserProfilePictureInLine(admin.TabularInline):
+    model = UserProfilePicture
+    extra = 0
+
+
+class ProjectInLine(admin.TabularInline):
+    model = Project
+    extra = 0
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "p_language",
+        "location",
+        "github_username",
+        "github_url",
+        "linked_in",
+        "portfolio",
+        "date_joined",
+        "last_login",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+    ]
+    inlines = [UserProfilePictureInLine]
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fields = ["title", "description", "user"]
+
+
+class ProgrammingLanguageAdmin(admin.ModelAdmin):
+    fields = ["language"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProgrammingLanguage, ProgrammingLanguageAdmin)
