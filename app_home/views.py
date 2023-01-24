fdafdd24ab123adb6f647fc6d73272c448b80a23
dragon-fire-fly <ek93@hotmail.co.uk>
2023-01-24 19:46:43 +0100

    Add HomeView and set up get_context_data

--- app_home/views.py
+++ app_home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import User, UserProfilePicture, Project, ProgrammingLanguage
 
 # Create your views here.
 
 
 class HomeView(TemplateView):
+    model = User
     template_name = "app_home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
