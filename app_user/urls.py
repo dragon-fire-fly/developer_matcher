fb36e08f8a75f337c062f67ff38a5a486e576574
dragon-fire-fly <ek93@hotmail.co.uk>
2023-01-25 16:10:43 +0100

    Add basic sign up template and view

--- app_user/urls.py
+++ app_user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from .views import SignupView
+
+app_name = "app_user"
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
+    path("success/", TemplateView.as_view(template_name="app_user/success.html"), name="success"),
 ]
--- app_user/views.py
+++ app_user/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import TemplateView, FormView
+from django.contrib.auth import login
 from .models import User, UserProfilePicture, Project, ProgrammingLanguage
 from .forms import UserSignupForm
 
 
-class SignupView(FormView):
-    # model = User
+class SignupView(TemplateView):
+    model = User
     template_name = "app_user/signup.html"
-    form_class = UserSignupForm
+
+    def post(self, request, *args, **kwargs):
+        signup_form = UserSignupForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            # automatically log the user in
+            login(request, user)
+            return redirect(reverse("app_user:success"))
+        return redirect(reverse("app_user:signup"))
+
+    def get(self, request, *args, **kwargs):
+        signup_form = UserSignupForm
+
+        # show user signup page
+        return render(
+            request,
+            "app_user/signup.html",
+            {
+                "signup_form": signup_form,
+            },
+        )
