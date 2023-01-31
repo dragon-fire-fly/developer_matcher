8a5496cccaababe09bfd7c50f9136b9646d1b8d4
dragon-fire-fly <ek93@hotmail.co.uk>
2023-01-31 14:26:24 +0100

    FEAT: Add basic template views for main pages

--- app_home/urls.py
+++ app_home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AboutView, DeveloperOverview, ProjectOverview
 
 app_name = "app_home"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="index"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("developers/", DeveloperOverview.as_view(), name="developer-overview"),
+    path("projects/", ProjectOverview.as_view(), name="project-overview"),
 ]
