e73f1556e26c80f5213d77343d9cec3b58bb1f6b
dragon-fire-fly <ek93@hotmail.co.uk>
2023-02-03 18:02:09 +0100

    FEAT: Add UserEditForm & basic profile update page

--- app_user/views.py
+++ app_user/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views.generic import TemplateView, FormView
 from django.contrib.auth import login
 from .models import User, UserProfilePicture, Project, ProgrammingLanguage
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserEditForm
 
 
 class RegisterView(TemplateView):
@@ -42,3 +42,19 @@ class ProfileView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+    def post(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)
+        return redirect(reverse("app_user:edit-profile"))
+
+
+class EditProfileView(TemplateView):
+    model = User
+    template_name = "app_user/user_profile_edit.html"
+
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)
+
+        context = {"form": UserEditForm(instance=user)}
+
+        return render(request, "app_user/user_profile_edit.html", context)
