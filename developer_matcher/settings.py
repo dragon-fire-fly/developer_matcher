85bd4df2fc1c21d706edcb60bfec333a13850ea8
dragon-fire-fly <ek93@hotmail.co.uk>
2023-02-05 14:56:03 +0100

    Fix: Attempt fix again for github workflow issue

--- developer_matcher/settings.py
+++ developer_matcher/settings.py
@@ -37,7 +37,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+if os.getenv("GITHUB_WORKFLOW"):
+    SECRET_KEY = "MySuperInsecureSecretKeyOnlyForGithubActionsThatIsLongerThan50Characters"
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
