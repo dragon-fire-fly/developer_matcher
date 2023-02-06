e7a1088b790e8b3f59b04fe7c0c2969b7bcbc351
dragon-fire-fly <ek93@hotmail.co.uk>
2023-02-06 17:42:22 +0100

    TEST: Add basic user login tests

--- app_user/tests/test_user_views.py
+++ app_user/tests/test_user_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 from datetime import date, datetime
 from app_user.models import User
@@ -21,25 +22,13 @@ from app_user.models import User
 
 class TestUserRegistration(TestCase):
     def setUp(self):
-        self.user = User.objects.create(
-            username="user_created_for_testing",
-            password="T35tP@55w0rd",
-            first_name="Test",
-            last_name="User",
-            email="test@test.com",
-            date_joined=date(2023, 1, 1),
-            github_username="test-user",
-            github_url="https://github.com/testuser",
-            linked_in="https://www.linkedin.com/in/test-user/",
-            portfolio="https://www.test.com/",
-        )
         # set up a client
         client = Client()
 
     def test_user_registration(self):
         # Test number of users before form submission
         self.assertEqual(
-            User.objects.count(), 1)
+            User.objects.count(), 0)
 
         # define url for registration page
         self.url = reverse("app_user:register")
@@ -58,7 +47,7 @@ class TestUserRegistration(TestCase):
         response = self.client.post(
             self.url,
             data={
-                "username": "user_created_for_testing_2",
+                "username": "user_created_for_testing",
                 "email": "test@test.com",
                 "password1": "T35tP@55w0rd",
                 "password2": "T35tP@55w0rd",
@@ -72,10 +61,10 @@ class TestUserRegistration(TestCase):
 
         # check if user was added to the database
         self.assertEqual(
-            User.objects.count(), 2)
+            User.objects.count(), 1)
 
         newly_created_user = User.objects.get(
-                username="user_created_for_testing_2"
+                username="user_created_for_testing"
             )
         # check user logged in and authenticated after signup
         self.assertTrue(
@@ -90,7 +79,7 @@ class TestUserRegistration(TestCase):
     def test_incorrect_user_registration(self):
         # Test number of users before form submission
         self.assertEqual(
-            User.objects.count(), 1)
+            User.objects.count(), 0)
 
         # define url for registration page
         self.url = reverse("app_user:register")
@@ -98,7 +87,7 @@ class TestUserRegistration(TestCase):
         response = self.client.post(
             self.url,
             data={
-                "username": "user_created_for_testing_3",
+                "username": "user_created_for_testing",
                 "email": "test@test.com",
                 "password1": "T35tP@55w0rd",
                 "password2": "1nc0rr3ctT35tP@55w0rd",
@@ -108,7 +97,7 @@ class TestUserRegistration(TestCase):
         # test that no new user was created (number of users after
         # form submission)
         self.assertEqual(
-            User.objects.count(), 1)
+            User.objects.count(), 0)
 
         # test that browser redirects back to the form for resubmission
         self.assertEqual(response.status_code, 302)
@@ -116,20 +105,77 @@ class TestUserRegistration(TestCase):
 
         # check for feedback if account creation failure
 
-        def test_user_login(self):
-            # - user can log in with existing user
-            self.client.force_login(user=user)
-            # - user receives feedback if login has failed
-            # (e.g. incorrect password)
-            pass
 
-        def test_user_login_google(self):
-            # - test user can sign up/in with google
-            pass
+class TestUserLogin(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="user_created_for_testing",
+            password="T35tP@55w0rd",
+            first_name="Test",
+            last_name="User",
+            email="test@test.com",
+            date_joined=date(2023, 1, 1),
+            github_username="test-user",
+            github_url="https://github.com/testuser",
+            linked_in="https://www.linkedin.com/in/test-user/",
+            portfolio="https://www.test.com/",
+        )
+        # set up a client
+        client = Client()
+        self.client.logout()
+        # define url for registration page
+        self.url = "/user/login/"
 
-        def test_user_login_github(self):
-            # - test user can sign up/in with github
-            pass
+    def test_user_login(self):
+
+        # Test GET route:
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/login.html")
+
+        # Test POST route: user can log in with existing user
+        response = self.client.post(
+            self.url,
+            data={
+                "username": "user_created_for_testing",
+                "password": "T35tP@55w0rd",
+                "password2": "T35tP@55w0rd",
+            },
+        )
+        test_user = User.objects.get(username="user_created_for_testing")
+        self.assertTrue(test_user.is_authenticated)
+
+        # self.client.force_login(user=user)
+        # - user receives feedback if login has failed
+        # (e.g. incorrect password)
+
+    def test_user_login_with_wrong_credentials(self):
+        response = self.client.post(
+            self.url,
+            data={
+                "username": "user_not_created_for_testing",
+                "password": "T35tP@55w0rd",
+                "password2": "T35tP@55w0rd",
+            },
+        )
+
+
+class TestGoogleLogin(TestCase):
+    def setUp(self):
+        pass
+
+    def test_user_login_google(self):
+        # test user can sign up/in with google
+        pass
+
+
+class TestGithubLogin(TestCase):
+    def setUp(self):
+        pass
+
+    def test_user_login_github(self):
+        # - test user can sign up/in with github
+        pass
 
 
 if __name__ == "__main__":
