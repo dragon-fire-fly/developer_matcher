9e5e6e90830cfe2e252a541fdafc1eb432a27b2d
dragon-fire-fly <ek93@hotmail.co.uk>
2023-02-26 16:35:19 +0100

    FIX: Issue set up a CI in Github Actions #25

--- app_home/forms.py
+++ app_home/forms.py
@@ -33,28 +33,9 @@ def validate_project_name(data):
 
 
 class ProjectCreationForm(forms.ModelForm):
-    # p_language_objects = ProgramLang.objects.all()
-    # lang_choices = []
-    # n = 0
-    # for lang in p_language_objects:
-    #     n += 1
-    #     lang_choices.append((n, lang.language))
-
-    # p_language = forms.MultipleChoiceField(
-    #     widget=forms.CheckboxSelectMultiple, choices=lang_choices
-    # )
-
     class Meta:
         model = Project
         fields = ["p_language", "title", "description"]
-
-    # def __init__(self, *args, **kwargs):
-    #     super(UserEditForm, self).__init__(*args, **kwargs)
-    #     p_lang_q_set = ProgramLang.objects.filter(user=self.instance)
-    #     p_langs_init = []
-    #     for lang in p_lang_q_set:
-    #         p_langs_init.append(lang.pk)
-    #     self.initial["p_language"] = p_langs_init
 
     def save(self, commit=True):
         project = super(ProjectCreationForm, self).save(commit=False)
@@ -67,28 +48,9 @@ class ProjectCreationForm(forms.ModelForm):
 
 
 class ProjectEditForm(forms.ModelForm):
-    # p_language_objects = ProgramLang.objects.all()
-    # lang_choices = []
-    # n = 0
-    # for lang in p_language_objects:
-    #     n += 1
-    #     lang_choices.append((n, lang.language))
-
-    # p_language = forms.MultipleChoiceField(
-    #     widget=forms.CheckboxSelectMultiple, choices=lang_choices
-    # )
-
     class Meta:
         model = Project
         fields = ["p_language", "title", "description"]
-
-    # def __init__(self, *args, **kwargs):
-    #     super(UserEditForm, self).__init__(*args, **kwargs)
-    #     p_lang_q_set = ProgramLang.objects.filter(user=self.instance)
-    #     p_langs_init = []
-    #     for lang in p_lang_q_set:
-    #         p_langs_init.append(lang.pk)
-    #     self.initial["p_language"] = p_langs_init
 
     def save(self, commit=True):
         project = super(ProjectEditForm, self).save(commit=False)
