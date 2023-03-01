0e0ebf3efafab7754777189faea3d2f6a866a1e2
dragon-fire-fly <ek93@hotmail.co.uk>
2023-03-01 11:09:45 +0100

    TEST: Add tests for user model str & json methods

---
+++ app_home/tests/test_home_models.py
@@ -0,0 +1,85 @@
+from django.test import TestCase
+from unittest import mock
+from django.urls import reverse
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from app_user.models import (
+    User,
+    ProgramLang,
+    Project,
+    UserProfilePicture,
+    ProjectPicture,
+)
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="testuser",
+            password="@t3s7p4ss!",
+            email="testuser@example.com",
+            first_name="Test",
+            last_name="User",
+            location="",
+            github_username="testuser",
+            github_url="https://github.com/testuser",
+            linked_in="https://www.linkedin.com/in/testuser",
+            portfolio="https://testuser.github.io",
+        )
+
+        self.another_user = User.objects.create(
+            username="anotheruser",
+            password="4n0th3rp4ss",
+            email="anotheruser@example.com",
+            first_name="Another",
+            last_name="User",
+            location="",
+            github_username="anotheruser",
+            github_url="https://github.com/anotheruser",
+            linked_in="https://www.linkedin.com/in/anotheruser",
+            portfolio="https://anotheruser.github.io",
+        )
+
+        self.user.follows.add(self.another_user)
+        self.another_user.follows.add(self.user)
+
+        self.python = ProgramLang.objects.create(
+            language="Python", language_icon=""
+        )
+        self.java = ProgramLang.objects.create(
+            language="Java", language_icon=""
+        )
+
+        self.javascript = ProgramLang.objects.create(
+            language="JavaScript", language_icon="path/to/javascript/icon.png"
+        )
+
+        self.user.p_language.add(self.python)
+        self.user.p_language.add(self.java)
+
+    def test_user_str_method(self):
+        self.assertEqual(str(self.user), "<user: testuser>")
+
+    def test_user_to_json_method(self):
+        expected_output = {
+            "username": "testuser",
+            "full name": "Test User",
+            "email": "testuser@example.com",
+            "location": "",
+            "github username": "testuser",
+            "github url": "https://github.com/testuser",
+            "linked in url": "https://www.linkedin.com/in/testuser",
+            "portfolio url": "https://testuser.github.io",
+        }
+        self.assertEqual(self.user.to_json(), expected_output)
+
+    def test_user_to_json_list_method(self):
+        expected_output = {
+            "programming languages": ["Python", "Java"],
+            "p_language_icons": [
+                "http://res.cloudinary.com/djlm3llv5/image/upload/p_language_icon",
+                "http://res.cloudinary.com/djlm3llv5/image/upload/p_language_icon",
+            ],
+        }
+        self.assertEqual(self.user.to_json_list(), expected_output)
