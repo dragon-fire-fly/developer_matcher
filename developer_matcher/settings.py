853a3bbf3679e53e68cf2c2caadbd22edc59bfe3
dragon-fire-fly <ek93@hotmail.co.uk>
2023-03-04 16:15:14 +0100

    FIX: Fix registration page not loading bug & style

--- developer_matcher/settings.py
+++ developer_matcher/settings.py
@@ -203,23 +203,22 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
-            "django.contrib.auth.password_validation."
-            "UserAttributeSimilarityValidator",
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa
         )
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation.MinimumLengthValidator"
+            "django.contrib.auth.password_validation.MinimumLengthValidator"  # noqa
         ),
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation.CommonPasswordValidator"
+            "django.contrib.auth.password_validation.CommonPasswordValidator"  # noqa
         ),
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation.NumericPasswordValidator"
+            "django.contrib.auth.password_validation.NumericPasswordValidator"  # noqa
         ),
     },
 ]
