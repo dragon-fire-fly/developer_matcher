e6c36fec52fa68081f35a8fab9a05b146deb77ba
dragon-fire-fly <ek93@hotmail.co.uk>
2023-03-16 12:25:59 +0100

    DOCS: Update colour scheme, remove notes sections

--- developer_matcher/settings.py
+++ developer_matcher/settings.py
@@ -15,9 +15,6 @@ import os
 import dj_database_url
 import cloudinary
 
-# import cloudinary.uploader
-# import cloudinary.api
-
 if os.path.isfile("env.py"):
     import env
 
@@ -33,7 +30,6 @@ cloudinary.config(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -178,18 +174,6 @@ else:
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-# "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
